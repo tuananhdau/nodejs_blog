5a4b0f46f56016cad16101abbaac69b25dfcf744
tuananhdau <anh_2351220058@dau.edu.vn>
2025-06-12 21:34:32 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -9,18 +9,18 @@ const port = 3000
 const hdb = require('express-handlebars').engine;
 
 const path = require('path');
-
+app.use(express.static(path.join(__dirname, 'public')))
 //Http logger
 
 app.use(morgan('combined'))
 
 //template engine
 
-app.engine('.handlebars', hdb({extname: '.handlebars'}));
+app.engine('.hbs', hdb({extname: '.hbs'}));
 
-app.set('view engine', '.handlebars');
+app.set('view engine', '.hbs');
 
-app.set('views', path.join(__dirname,'resources//views'));
+app.set('views', path.join(__dirname,'resources/views'));
 
 // console.log(path.join(__dirname,'resources//views'))
 
